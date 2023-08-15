94e9b606db0779c4d9d684289829ddf792b40bcb
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-08-15 16:30:52 +0530

    existing service job

--- Minella/Spring-Batch-by-Michael-Minella/38-existing-service-job/src/main/java/com/example/ExistingServiceJobListener.java
+++ Minella/Spring-Batch-by-Michael-Minella/38-existing-service-job/src/main/java/com/example/ExistingServiceJobListener.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Component;
 public class ExistingServiceJobListener implements JobExecutionListener {
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        System.out.println("ExistingServiceJobListener | beforeJob executed | JobId = " + jobExecution.getJobId());
+        System.out.println("===== ExistingServiceJobListener | beforeJob executed | JobId = " + jobExecution.getJobId());
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        System.out.println("ExistingServiceJobListener | afterJob executed | JobId = " + jobExecution.getJobId());
+        System.out.println("==== ExistingServiceJobListener | afterJob executed | JobId = " + jobExecution.getJobId());
         System.out.println("-----------------------");
         System.out.println("STATUS     : " + jobExecution.getStatus());
         System.out.println("START_TIME : " + jobExecution.getStartTime());
