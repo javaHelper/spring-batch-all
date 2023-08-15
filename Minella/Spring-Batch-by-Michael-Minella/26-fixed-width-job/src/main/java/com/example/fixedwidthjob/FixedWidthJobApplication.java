bed60cbfa3a861c04c067e2025f9aed8f5f464b6
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-08-15 10:51:06 +0530

    fixed width job

--- Minella/Spring-Batch-by-Michael-Minella/26-fixed-width-job/src/main/java/com/example/fixedwidthjob/FixedWidthJobApplication.java
+++ Minella/Spring-Batch-by-Michael-Minella/26-fixed-width-job/src/main/java/com/example/fixedwidthjob/FixedWidthJobApplication.java
@@ -40,7 +40,7 @@ public class FixedWidthJobApplication {
                 .columns(new Range[]{new Range(1, 11), new Range(12, 12), new Range(13, 22),
                         new Range(23, 26), new Range(27, 46), new Range(47, 62), new Range(63, 64),
                         new Range(65, 69)})
-                .names(new String[]{"firstName", "middleInitial", "lastName", "addressNumber", "street", "city", "state", "zipCode"})
+                .names("firstName", "middleInitial", "lastName", "addressNumber", "street", "city", "state", "zipCode")
                 .targetType(Customer.class)
                 .build();
     }
