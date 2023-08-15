6ba148da2d4558ddc4eb01bacbf10420a7397cbc
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-08-15 17:04:22 +0530

    retry

---
+++ Minella/Spring-Batch-by-Michael-Minella/45-retry/src/main/java/com/example/demo/writer/RetryItemWriter.java
@@ -0,0 +1,40 @@
+package com.example.demo.writer;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+
+import com.example.demo.exception.CustomRetryableException;
+
+public class RetryItemWriter implements ItemWriter<String> {
+
+	private boolean retry = false;
+	private int attemptCount = 0;
+
+	@Override
+	public void write(List<? extends String> items) throws Exception {
+		for (String item : items) {
+			System.out.println("writing item " + item);
+			if(retry && item.equalsIgnoreCase("-84")) {
+				attemptCount++;
+
+				if(attemptCount >= 5) {
+					System.out.println("Success!");
+					retry = false;
+					System.out.println(item);
+				}
+				else {
+					System.out.println("Writing of item " + item + " failed");
+					throw new CustomRetryableException("Write failed.  Attempt:" + attemptCount);
+				}
+			}
+			else {
+				System.out.println(item);
+			}
+		}
+	}
+
+	public void setRetry(boolean retry) {
+		this.retry = retry;
+	}
+}
