b4039db5c9b1993b13056240a401d84a601c43ee
Prateek <54174687+javaHelper@users.noreply.github.com>
2024-08-11 17:18:47 +0530

    Clean up!

--- Minella/Spring-Batch-by-Michael-Minella/spring-batch-metadata-in-diff-schema/target/classes/schema-mysql.sql
+++
@@ -1,7 +0,0 @@
-create table Customer (
-       id bigint not null auto_increment,
-        birthdate varchar(255),
-        firstName varchar(255),
-        lastName varchar(255),
-        primary key (id)
-    ) engine=InnoDB;
--- Minella/Spring-Batch-by-Michael-Minella/spring-boot-batch-pass-list-values-to-sql/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into person values (1, 'foo');
-insert into person values (2, 'bar');
-insert into person values (3, 'baz');
