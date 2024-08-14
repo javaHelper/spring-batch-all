dff4d98e3f6100791a4188a395b6bd56bd934a2c
Prateek <54174687+javaHelper@users.noreply.github.com>
2024-08-14 23:10:13 +0530

    ClassifierCompositeItemWriter

--- Minella/Spring-Batch-by-Michael-Minella/21-classifierWrittingMultipleDestinations/src/main/java/com/example/mapper/CustomerRowMapper.java
+++ Minella/Spring-Batch-by-Michael-Minella/21-classifierWrittingMultipleDestinations/src/main/java/com/example/mapper/CustomerRowMapper.java
@@ -10,7 +10,8 @@ import com.example.model.Customer;
 public class CustomerRowMapper implements RowMapper<Customer> {
 	@Override
 	public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-		return Customer.builder().id(rs.getLong("id"))
+		return Customer.builder()
+				.id(rs.getLong("id"))
 				.firstName(rs.getString("firstName"))
 				.lastName(rs.getString("lastName"))
 				.birthdate(rs.getString("birthdate")).build();
