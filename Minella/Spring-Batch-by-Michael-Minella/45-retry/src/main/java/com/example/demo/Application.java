ab9ba04ee3ef0a6563930e155cf1475af8e88450
Prateek <54174687+javaHelper@users.noreply.github.com>
2024-08-15 10:59:03 +0530

    Retry

--- Minella/Spring-Batch-by-Michael-Minella/45-retry/src/main/java/com/example/demo/Application.java
+++ Minella/Spring-Batch-by-Michael-Minella/45-retry/src/main/java/com/example/demo/Application.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @EnableBatchProcessing
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
