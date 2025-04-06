599c23a51556f164ecee4ab4682eb838b7d37100
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-06 22:39:15 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/48-asyncItemProcessorItemWriter/src/main/java/com/example/configuration/JobConfiguration.java
+++ Minella/Spring-Batch-by-Michael-Minella/48-asyncItemProcessorItemWriter/src/main/java/com/example/configuration/JobConfiguration.java
@@ -1,15 +1,12 @@
 package com.example.configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-
-import javax.sql.DataSource;
-
+import com.example.domain.Customer;
+import com.example.mapper.CustomerRowMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.integration.async.AsyncItemProcessor;
 import org.springframework.batch.integration.async.AsyncItemWriter;
 import org.springframework.batch.item.ItemProcessor;
@@ -24,19 +21,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
-import com.example.domain.Customer;
-import com.example.mapper.CustomerRowMapper;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 
 @SuppressWarnings({"rawtypes","unchecked"})
 @Configuration
 public class JobConfiguration {
 	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+	private JobRepository jobRepository;
 	
 	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
+	private PlatformTransactionManager manager;
 	
 	@Autowired
 	private DataSource dataSource;
@@ -88,7 +88,7 @@ public class JobConfiguration {
 	public JdbcBatchItemWriter<Customer> customerItemWriter(){
 		return new JdbcBatchItemWriterBuilder<Customer>()
 				.dataSource(this.dataSource)
-				.sql("INSERT INTO NEW_CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)")
+				.sql("INSERT INTO new_customer VALUES (:id, :firstName, :lastName, :birthdate)")
 				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
 				.build();
 	}
@@ -103,8 +103,8 @@ public class JobConfiguration {
 	
 	@Bean
 	public Step step1() throws Exception {
-		return stepBuilderFactory.get("step1")
-				.chunk(1000)
+		return new StepBuilder("step1", jobRepository)
+				.chunk(1000, manager)
 				.reader(customerPagingItemReader())
 				.processor(asyncItemProcessor())
 				.writer(asyncItemWriter())
@@ -113,7 +113,7 @@ public class JobConfiguration {
 	
 	@Bean
 	public Job job() throws Exception {
-		return jobBuilderFactory.get("job")
+		return new JobBuilder("job", jobRepository)
 				.start(step1())
 				.build();
 	}
