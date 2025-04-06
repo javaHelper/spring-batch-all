a7899d5bb1ee9fccc92ffa57c85a498e3d738463
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 01:59:47 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/41-itemValidation/src/main/java/com/example/configuration/JobConfiguration.java
+++ Minella/Spring-Batch-by-Michael-Minella/41-itemValidation/src/main/java/com/example/configuration/JobConfiguration.java
@@ -1,16 +1,14 @@
 package com.example.configuration;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
+import com.example.aggregator.CustomLineAggregator;
+import com.example.mapper.CustomerRowMapper;
+import com.example.model.Customer;
+import com.example.validation.CustomerValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
@@ -22,21 +20,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
-import com.example.aggregator.CustomLineAggregator;
-import com.example.mapper.CustomerRowMapper;
-import com.example.model.Customer;
-import com.example.validation.CustomerValidator;
+import javax.sql.DataSource;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
-
-@EnableBatchProcessing
 @Configuration
 public class JobConfiguration {
 	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+	private JobRepository jobRepository;
 	
 	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
+	private PlatformTransactionManager manager;
 	
 	@Autowired
 	private DataSource dataSource;
@@ -86,8 +83,8 @@ public class JobConfiguration {
 	
 	@Bean
 	public Step step1() throws Exception {
-		return stepBuilderFactory.get("step1")
-				.<Customer, Customer>chunk(100)
+		return new StepBuilder("step1", jobRepository)
+				.<Customer, Customer>chunk(100, manager)
 				.reader(customerPagingItemReader())
 				.processor(itemProcessor())
 				.writer(customerItemWriter())
@@ -96,7 +93,7 @@ public class JobConfiguration {
 	
 	@Bean
 	public Job job() throws Exception {
-		return jobBuilderFactory.get("job")
+		return new JobBuilder("job", jobRepository)
 				.start(step1())
 				.build();
 	}
