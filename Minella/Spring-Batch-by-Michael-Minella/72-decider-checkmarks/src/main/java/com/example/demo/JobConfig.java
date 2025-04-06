72105b2af570c94f50d219ba3944d4c43cca746d
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-06 15:50:08 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/72-decider-checkmarks/src/main/java/com/example/demo/JobConfig.java
+++ Minella/Spring-Batch-by-Michael-Minella/72-decider-checkmarks/src/main/java/com/example/demo/JobConfig.java
@@ -4,86 +4,88 @@ import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class JobConfig {
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private JobRepository jobRepository;
 
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private PlatformTransactionManager manager;
 
     @Bean
     public Step validateCsvHeaderStepStep(){
-        return stepBuilderFactory.get("validateCsvHeaderStepStep")
+        return new StepBuilder("validateCsvHeaderStepStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("validateCsvHeaderStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
     public Step processCsvStep(){
-        return stepBuilderFactory.get("processCsvStep")
+        return new StepBuilder("processCsvStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("processCsvStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
     public Step markCsvAsFailedStep(){
-        return stepBuilderFactory.get("markCsvAsFailedStep")
+        return new StepBuilder("markCsvAsFailedStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("markCsvAsFailedStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
     public Step moveFailedCsvStep(){
-        return stepBuilderFactory.get("moveFailedCsvStep")
+        return new StepBuilder("moveFailedCsvStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("moveFailedCsvStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
     public Step moveCsvStep(){
-        return stepBuilderFactory.get("moveCsvStep")
+        return new StepBuilder("moveCsvStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("moveCsvStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
     public Job job(){
-        return jobBuilderFactory.get("job")
+        return new JobBuilder("job", jobRepository)
                 .flow(validateCsvHeaderStepStep())
                 	.on(ExitStatus.FAILED.getExitCode()).to(markCsvAsFailedStep())
                 .from(validateCsvHeaderStepStep())
