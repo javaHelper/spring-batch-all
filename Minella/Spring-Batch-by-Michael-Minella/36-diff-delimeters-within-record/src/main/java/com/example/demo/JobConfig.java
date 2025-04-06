8c1bbb1b5868c0621f097cf8bb2fc52002962f66
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-06 13:29:10 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/36-diff-delimeters-within-record/src/main/java/com/example/demo/JobConfig.java
+++ Minella/Spring-Batch-by-Michael-Minella/36-diff-delimeters-within-record/src/main/java/com/example/demo/JobConfig.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
@@ -13,13 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class JobConfig {
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private JobRepository jobRepository;
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private PlatformTransactionManager manager;
 
     @Bean
     public FlatFileItemReader<Person> flatFileItemReader(){
@@ -43,8 +45,8 @@ public class JobConfig {
 
     @Bean
     public Step step1(){
-        return stepBuilderFactory.get("step1")
-                .<Person, Person>chunk(2)
+        return new StepBuilder("step1", jobRepository)
+                .<Person, Person>chunk(2, manager)
                 .reader(flatFileItemReader())
                 .writer(itemWriter())
                 .build();
@@ -52,7 +54,7 @@ public class JobConfig {
 
     @Bean
     public Job job(){
-        return jobBuilderFactory.get("job")
+        return new JobBuilder("job", jobRepository)
                 .start(step1())
                 .build();
     }
