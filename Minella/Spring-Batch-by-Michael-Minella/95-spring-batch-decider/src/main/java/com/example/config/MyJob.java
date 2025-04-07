9ab45965d0817ecf81511c52562635f441e25f79
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 12:43:54 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/95-spring-batch-decider/src/main/java/com/example/config/MyJob.java
+++ Minella/Spring-Batch-by-Michael-Minella/95-spring-batch-decider/src/main/java/com/example/config/MyJob.java
@@ -1,72 +1,77 @@
 package com.example.config;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.example.decider.MyJobExecutionDecider;
 import com.example.tasklet.Step1Tasklet;
 import com.example.tasklet.Step2Tasklet;
 import com.example.tasklet.Step3Tasklet;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 public class MyJob {
 
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private JobRepository jobRepository;
 
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private PlatformTransactionManager manager;
 
-	@Bean
-	public Step1Tasklet step1Tasklet() {
-		return new Step1Tasklet();
-	}
+    @Bean
+    public Step1Tasklet step1Tasklet() {
+        return new Step1Tasklet();
+    }
 
-	@Bean
-	public Step2Tasklet step2Tasklet() {
-		return new Step2Tasklet();
-	}
-	
-	@Bean
-	public Step3Tasklet step3Tasklet() {
-		return new Step3Tasklet();
-	}
+    @Bean
+    public Step2Tasklet step2Tasklet() {
+        return new Step2Tasklet();
+    }
 
-	@Bean
-	public Step step1() {
-		return stepBuilderFactory.get("step1").tasklet(step1Tasklet()).build();
-	}
+    @Bean
+    public Step3Tasklet step3Tasklet() {
+        return new Step3Tasklet();
+    }
 
-	@Bean
-	public MyJobExecutionDecider decider() {
-		return new MyJobExecutionDecider();
-	}
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(step1Tasklet(), manager)
+                .build();
+    }
 
-	@Bean
-	public Step step2() {
-		return stepBuilderFactory.get("step2").tasklet(step2Tasklet()).build();
-	}
+    @Bean
+    public MyJobExecutionDecider decider() {
+        return new MyJobExecutionDecider();
+    }
 
-	@Bean
-	public Step step3() {
-		return stepBuilderFactory.get("step3").tasklet(step3Tasklet()).build();
-	}
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet(step2Tasklet(), manager)
+                .build();
+    }
 
-	@Bean
-	public Job job() {
-		return jobBuilderFactory.get("job")
-				.start(step1())
-				.next(decider())
-					.on("YES").to(step2())
-				.from(decider())
-					.on("NO").to(step3()).end()
-				.build();
-	}
+    @Bean
+    public Step step3() {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet(step3Tasklet(), manager)
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(step1())
+                .next(decider())
+                .on("YES").to(step2())
+                .from(decider())
+                .on("NO").to(step3()).end()
+                .build();
+    }
 }
