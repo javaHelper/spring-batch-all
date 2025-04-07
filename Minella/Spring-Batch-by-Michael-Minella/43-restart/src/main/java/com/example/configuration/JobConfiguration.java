64129c235c15709f01dd3711dbaf95c80ba6bce4
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 11:08:01 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/43-restart/src/main/java/com/example/configuration/JobConfiguration.java
+++ Minella/Spring-Batch-by-Michael-Minella/43-restart/src/main/java/com/example/configuration/JobConfiguration.java
@@ -1,27 +1,29 @@
 package com.example.configuration;
 
-import java.util.Map;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Map;
 
 @Configuration
 public class JobConfiguration {
 	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+	private JobRepository jobRepository;
 	
 	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
+	private PlatformTransactionManager manager;
 	
 	@Bean
 	@StepScope
@@ -46,21 +48,21 @@ public class JobConfiguration {
 	
 	@Bean
 	public Step step1() {
-		return stepBuilderFactory.get("step1")
-				.tasklet(restartTasklet())
+		return new StepBuilder("step1", jobRepository)
+				.tasklet(restartTasklet(), manager)
 				.build();
 	}
 	
 	@Bean
 	public Step step2() {
-		return stepBuilderFactory.get("step2")
-				.tasklet(restartTasklet())
+		return new StepBuilder("step2", jobRepository)
+				.tasklet(restartTasklet(), manager)
 				.build();
 	}
 	
 	@Bean
 	public Job job() {
-		return jobBuilderFactory.get("job")
+		return new JobBuilder("job", jobRepository)
 				.start(step1())
 				.next(step2())
 				.build();
