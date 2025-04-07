260c93263b982a0ad92ecde296a8c35bf3e6f0f0
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 12:00:57 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/86-conditional-flow-demo/src/main/java/com/example/demo/JobConfig.java
+++ Minella/Spring-Batch-by-Michael-Minella/86-conditional-flow-demo/src/main/java/com/example/demo/JobConfig.java
@@ -3,16 +3,18 @@ package com.example.demo;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class JobConfig {
@@ -21,13 +23,13 @@ public class JobConfig {
     }
 
     @Autowired
-    private JobBuilderFactory jobs;
+    private JobRepository jobRepository;
     @Autowired
-    private StepBuilderFactory steps;
+    private PlatformTransactionManager manager;
 
     @Bean
-    public Step retrieveFileStep(){
-        return steps.get("retrieveFileStep")
+    public Step retrieveFileStep() {
+        return new StepBuilder("retrieveFileStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -35,35 +37,35 @@ public class JobConfig {
                         chunkContext.getStepContext().getStepExecution().getExecutionContext().put("type", Type.YEARLY);
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
-    public Step yearlyStep(){
-        return steps.get("yearlyStep")
+    public Step yearlyStep() {
+        return new StepBuilder("yearlyStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("running yearlyStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
-    public Step quarterlyStep(){
-        return steps.get("quarterlyStep")
+    public Step quarterlyStep() {
+        return new StepBuilder("quarterlyStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("running quarterlyStep");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
-    public JobExecutionDecider fileMapperDecider(){
+    public JobExecutionDecider fileMapperDecider() {
         return (jobExecution, stepExecution) -> {
             Type type = (Type) stepExecution.getExecutionContext().get("type");
             return new FlowExecutionStatus(type == Type.YEARLY ? "yearly" : "quarterly");
@@ -72,15 +74,14 @@ public class JobConfig {
 
     @Bean
     public Job job() {
-        return jobs.get("job")
+        return new JobBuilder("job", jobRepository)
                 .start(retrieveFileStep())
                 .next(fileMapperDecider())
                 .from(fileMapperDecider())
-                	.on("yearly").to(yearlyStep())
+                .on("yearly").to(yearlyStep())
                 .from(fileMapperDecider())
-                	.on("quarterly").to(quarterlyStep())
-                	.build()
+                .on("quarterly").to(quarterlyStep())
+                .build()
                 .build();
     }
 }
-
