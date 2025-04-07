04fb931e8f9f411ff1f2f44d01d3504074b6d861
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 10:56:42 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/80-item-writer-adapter/src/main/java/com/example/MyJobConfig.java
+++ Minella/Spring-Batch-by-Michael-Minella/80-item-writer-adapter/src/main/java/com/example/MyJobConfig.java
@@ -1,19 +1,28 @@
 package com.example;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.adapter.ItemWriterAdapter;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class MyJobConfig {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private PlatformTransactionManager manager;
 
 	@Bean
     public ItemReader<User> itemReader() {
@@ -30,10 +39,10 @@ public class MyJobConfig {
     }
 	
 	@Bean
-    public Job job(JobBuilderFactory jobs, StepBuilderFactory steps) {
-        return jobs.get("job")
-                .start(steps.get("step")
-                        .<User, User>chunk(5)
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(new StepBuilder("step", jobRepository)
+                        .<User, User>chunk(5, manager)
                         .reader(itemReader())
                         .writer(itemWriter())
                         .build())
