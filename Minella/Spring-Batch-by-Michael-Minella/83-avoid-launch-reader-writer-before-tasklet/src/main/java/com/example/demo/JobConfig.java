48cf294e3b717cb7072883de294a4c7317bc0543
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 11:49:10 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/83-avoid-launch-reader-writer-before-tasklet/src/main/java/com/example/demo/JobConfig.java
+++ Minella/Spring-Batch-by-Michael-Minella/83-avoid-launch-reader-writer-before-tasklet/src/main/java/com/example/demo/JobConfig.java
@@ -3,9 +3,10 @@ package com.example.demo;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
@@ -13,19 +14,20 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Arrays;
 
 @Configuration
 public class JobConfig {
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private JobRepository jobRepository;
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private PlatformTransactionManager manager;
 
     @Bean
     public Step validateStep(){
-        return stepBuilderFactory.get("validateStep")
+        return new StepBuilder("validateStep", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -39,7 +41,7 @@ public class JobConfig {
                         // TODO implement validation logic
                         return false;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
@@ -56,8 +58,8 @@ public class JobConfig {
 
     @Bean
     public Step readAndWriteCsvFile(){
-        return stepBuilderFactory.get("readAndWriteCsvFile")
-                .<Integer, Integer>chunk(2)
+        return new StepBuilder("readAndWriteCsvFile", jobRepository)
+                .<Integer, Integer>chunk(2, manager)
                 .reader(itemReader())
                 .writer(itemWriter())
                 .build();
@@ -65,7 +67,7 @@ public class JobConfig {
 
     @Bean
     public Job job(){
-        return jobBuilderFactory.get("job")
+        return new JobBuilder("job", jobRepository)
                 .start(validateStep())
                 .next(readAndWriteCsvFile())
                 .build();
