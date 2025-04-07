9d7d4710f451aa7cea6be37a648ff064dd1ea4e5
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-04-07 12:21:37 +0530

    Updated to Spring Boot 3.2.10

--- Minella/Spring-Batch-by-Michael-Minella/91-passing-tasklet-failure-message-to-next-step/src/main/java/com/example/demo/JobConfiguration.java
+++ Minella/Spring-Batch-by-Michael-Minella/91-passing-tasklet-failure-message-to-next-step/src/main/java/com/example/demo/JobConfiguration.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Collection;
 import java.util.List;
@@ -16,24 +22,24 @@ import java.util.List;
 @Configuration
 public class JobConfiguration {
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private PlatformTransactionManager manager;
 
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private JobRepository jobRepository;
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
+        return new StepBuilder("step1", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("hello");
                     throw new Exception("Boom!");
-                })
+                }, manager)
                 .build();
     }
 
     @Bean
     public Step step2() {
-        return stepBuilderFactory.get("step2")
+        return new StepBuilder("step2", jobRepository)
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -49,13 +55,13 @@ public class JobConfiguration {
                         System.out.println("Step2");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                }, manager).build();
     }
 
     @Bean
     public Job job(){
         // Obviously, you need to make sure step1 flows to step2 in all cases (Hence the flow definition).
-        return jobBuilderFactory.get("job")
+        return new JobBuilder("job", jobRepository)
                 .flow(step1())
                     .on("*").to(step2())
                     .build()
